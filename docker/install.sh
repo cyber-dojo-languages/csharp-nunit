34f4686264994b58660ad587e30963296f14fbcb
JonJagger <jon@kosli.com>
2026-02-11 06:15:10 +0000

    Update to new base image

--- docker/install.sh
+++ docker/install.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 set -Eeu
 
-# After these commands have run ~/.nuget/packages contains nunit/ and nunit.console/ dirs
-# and the jj dir has a csproj file copied into the docker/ dir
+# After the dotnet command has run ~/.nuget/packages contains nunit/  
+# so it is important the current user is sandbox.
+# Also, the /tmp dir has a csproj file (copied into the docker/ dir ready to be used
+# in the start-point)
 
+[ "$(whoami)" == sandbox ] || (>&2 echo 'User must be sandbox' ; kill -INT $$)
 cd /tmp
-mkdir jj
-
-dotnet new nunit --output jj
-cd jj
-dotnet add package NUnit.ConsoleRunner
-
-cd ..
-rm -rf *
+dotnet new nunit
